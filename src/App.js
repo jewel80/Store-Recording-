471719e6d024fa74cfdb697e5caafadfef6af828
Md. Jewel Rana <m.jewelrana80@gmail.com>
2021-11-03 23:32:47 +0600

    initial init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navber from './components/Navber';
+import Product from './components/Product';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+import ProductList from './components/ProductList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Navber />
+        <Switch>
+          <Route exact path="/" component={ProductList} />
+          <Route path="/details" component={Details} />
+          <Route path="/cart" component={Cart} />
+          <Route component={Default} /> 
+        </Switch>
+      </React.Fragment>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/components/Navber.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Navber extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Navber</h3>
+            </div>
+        )
+    }
+}
